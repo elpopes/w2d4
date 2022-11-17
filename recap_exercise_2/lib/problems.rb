1229fea8b261aff1b2c032bdf632b5d296fe03e4
lorenzo <lorenzotijerina@gmail.com>
2022-11-17 12:56:19 -0500

    complete rspec 2

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -1,45 +1,56 @@
-# Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
-# of both of the given numbers
 def least_common_multiple(num_1, num_2)
-
+    (1..num_1 * num_2).select {|num| num % num_1 == 0 && num % num_2 == 0}.min
 end
 
-
-# Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
-# most in the string.
 def most_frequent_bigram(str)
-
+    char_pairs(str).max_by {|pair| char_pairs(str).count(pair)}
 end
 
+
+def char_pairs(str)
+    str.split("")[0...-1].map.with_index{|char, i| char + str[i+1]}
+end
 
 class Hash
-    # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        switched = {}
+        each {|k,v| switched[v] = k}
+        switched
     end
 end
 
 
 class Array
-    # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
+  
     def pair_sum_count(num)
-
+        all_subs.count {|arrs| arrs.first + arrs.last == num }
     end
 
-    # Write a method, Array#bubble_sort, that takes in an optional proc argument.
-    # When given a proc, the method should sort the array according to the proc.
-    # When no proc is given, the method should sort the array in increasing order.
-    #
-    # Sorting algorithms like bubble_sort, commonly accept a block. That block accepts
-    # two parameters, which represents the two elements in the array being compared.
-    # If the block returns 1, it means that the second element passed to the block
-    # should go before the first (i.e. switch the elements). If the block returns -1,
-    # it means the first element passed to the block should go before the second
-    # (i.e. do not switch them). If the block returns 0 it implies that
-    # it does not matter which element goes first (i.e. do nothing).
-    #
-    # This should remind you of the spaceship operator! Convenient :)
-    def bubble_sort(&prc)
+    def all_subs
+        map.with_index do |num, i| 
+            map.with_index do |num2, j| 
+                if i < j
+                    [num, num2]
+                end
+            end.compact
+        end.flatten(1)
+    end
 
+    def bubble_sort(&prc)
+        prc ||= Proc.new {|a,b| a <=> b}
+        sorted = false
+        while !sorted 
+            sorted = true
+            each_with_index do |ele, i|
+                if i + 1 < length
+                    j = i+1
+                    if prc.call(ele, self[j]) == 1    
+                        self[i], self[i+1] = self[i+1], self[i]
+                        sorted = false
+                    end
+                end
+            end
+        end
+        self
     end
 end

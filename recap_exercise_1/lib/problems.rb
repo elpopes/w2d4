9096f559b569a1836a9e19aee30dbac02d9e0bc8
lorenzo <lorenzotijerina@gmail.com>
2022-11-17 10:34:52 -0500

    complete recap_ex_1

--- recap_exercise_1/lib/problems.rb
+++ recap_exercise_1/lib/problems.rb
@@ -1,83 +1,59 @@
-# Write a method, all_vowel_pairs, that takes in an array of words and returns all pairs of words
-# that contain every vowel. Vowels are the letters a, e, i, o, u. A pair should have its two words
-# in the same order as the original array. 
-#
-# Example:
-#
-# all_vowel_pairs(["goat", "action", "tear", "impromptu", "tired", "europe"])   # => ["action europe", "tear impromptu"]
+
 def all_vowel_pairs(words)
-
+    words[0...-1].map.with_index {|word, i| 
+        (i+1...words.length).map {|j| 
+            to_pair(word, words[j]) if complete_pairs?(to_pair(word, words[j]))}.compact}.flatten
 end
 
+def to_pair(word_1, word_2)
+    "#{word_1} #{word_2}"
+end
 
-# Write a method, composite?, that takes in a number and returns a boolean indicating if the number
-# has factors besides 1 and itself
-#
-# Example:
-#
-# composite?(9)     # => true
-# composite?(13)    # => false
+def complete_pairs?(combo_str)
+    "aeiou".each_char {|char| return false unless combo_str.include?(char)} 
+    true
+end
+
 def composite?(num)
-
+    (2...num).any? {|i| num % i == 0}
 end
 
 
-# A bigram is a string containing two letters.
-# Write a method, find_bigrams, that takes in a string and an array of bigrams.
-# The method should return an array containing all bigrams found in the string.
-# The found bigrams should be returned in the the order they appear in the original array.
-#
-# Examples:
-#
-# find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
-# find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
+    bigrams.select {|bi| char_pairs(str).include?(bi)}
+end
 
+def char_pairs(str)
+    str.split("")[0...-1].map.with_index{|char, i| char + str[i+1]}
 end
 
 class Hash
-    # Write a method, Hash#my_select, that takes in an optional proc argument
-    # The method should return a new hash containing the key-value pairs that return
-    # true when passed into the proc.
-    # If no proc is given, then return a new hash containing the pairs where the key is equal to the value.
-    #
-    # Examples:
-    #
-    # hash_1 = {x: 7, y: 1, z: 8}
-    # hash_1.my_select { |k, v| v.odd? }          # => {x: 7, y: 1}
-    #
-    # hash_2 = {4=>4, 10=>11, 12=>3, 5=>6, 7=>8}
-    # hash_2.my_select { |k, v| k + 1 == v }      # => {10=>11, 5=>6, 7=>8})
-    # hash_2.my_select                            # => {4=>4}
-    def my_select(&prc)
 
+    def my_select(&prc)
+        prc ||= Proc.new {|k,v| k == v}
+        select {|k, v| prc.call(k,v)}
     end
 end
 
 class String
-    # Write a method, String#substrings, that takes in a optional length argument
-    # The method should return an array of the substrings that have the given length.
-    # If no length is given, return all substrings.
-    #
-    # Examples:
-    #
-    # "cats".substrings     # => ["c", "ca", "cat", "cats", "a", "at", "ats", "t", "ts", "s"]
-    # "cats".substrings(2)  # => ["ca", "at", "ts"]
-    def substrings(length = nil)
 
+    def substrings(length = nil)
+        all_subs.select {|ele| length.nil? ? ele : ele.length == length}
+    end
+
+    def all_subs
+        self.split("").map.with_index{|ele, i| (i...length).map {|j| self[i..j]}}.flatten
     end
 
 
-    # Write a method, String#caesar_cipher, that takes in an a number.
-    # The method should return a new string where each char of the original string is shifted
-    # the given number of times in the alphabet.
-    #
-    # Examples:
-    #
-    # "apple".caesar_cipher(1)    #=> "bqqmf"
-    # "bootcamp".caesar_cipher(2) #=> "dqqvecor"
-    # "zebra".caesar_cipher(4)    #=> "difve"
+    require "byebug"
     def caesar_cipher(num)
+        split("").map.with_index {|char, i| char.encoded(num)}.join("")
+    end
 
+    def encoded(num)
+        alpha = ('a'..'z').to_a
+        new_index = alpha.index(self) + num
+        alpha[new_index % 26]
     end
 end
